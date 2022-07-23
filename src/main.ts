f471d7f23341396b0e420b3ee6f22c9aa437c8d3
Roman Nguyen <march08@outlook.com>
2022-07-23 22:50:55 +0200

    new build, remove custom defaults

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import App from "./App.svelte";
 const app = new App({
   target: document.getElementById("calculator"),
   props: {
-    options: {
-      initialValues: {
-        attendeeCount: 2,
-      },
-      defaultSalary: 0,
-    },
+    // options: {
+    //   initialValues: {
+    //     attendeeCount: 2,
+    //   },
+    //   defaultSalary: 0,
+    // },
   },
 });
 
