b241ea52f793989b2271cb9fed1c1addf1ff96cf
Roman Nguyen <march08@outlook.com>
2022-07-24 12:22:10 +0200

    disable scroll animation to fix ios

--- src/utils/scrollToCalculator.ts
+++ src/utils/scrollToCalculator.ts
@@ -1,11 +1,8 @@
 export const scrollToCalculator = (offset: number = -50) => {
   try {
-    window.scrollTo({
-      top:
-        document.getElementById("ott-meeting-cost-calculator").offsetTop +
-        offset,
-      left: 0,
-      behavior: "smooth",
-    });
+    window.scrollTo(
+      0,
+      document.getElementById("ott-meeting-cost-calculator").offsetTop + offset
+    );
   } catch {}
 };
